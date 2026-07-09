3a65c1199c98b9b66fd3418349cdeda9c5148781
Dawn952 <135291747+Dawn952@users.noreply.github.com>
2026-07-09 21:11:53 +0800

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -3,3 +3,14 @@ curl -X POST http://192.168.13.153:8900/v1/completions   -H "Content-Type: appli
            "prompt": "The quick brown fox jumps over the lazy dog",
           "max_tokens": 160
          }'
+
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{  
+"model": "kimi_k26",  
+"messages": [{  
+"role": "user",  
+"content": "The quick brown fox jumps over the lazy dog"  
+}],  
+"stream": false,  
+"ignore_eos": false,  
+"max_tokens": 200  
+}' http://80.5.17.104:8089/v1/chat/completions

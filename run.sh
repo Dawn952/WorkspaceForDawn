4ac8eb8735a0d8fa32d84ea2107bf5fd9141951a
Dawn952 <135291747+Dawn952@users.noreply.github.com>
2026-07-13 09:46:21 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -44,3 +44,84 @@ vllm serve $TARGET_MODEL \
     --mm-processor-cache-gb 0 \
     --mm-encoder-tp-mode data \
     --speculative-config '{"method": "dflash","model": "/mnt/weight/kimi_k2.5_dflash", "num_speculative_tokens": 15}'
+
+
+
+#!/bin/bash
+#source /mnt/share/s30078275/env_kimi.sh
+
+unset ftp_proxy
+unset https_proxy
+unset http_proxy
+
+nic_name="enp48s3u1u1"
+local_ip="141.xx.xx.xxx"
+
+export HCCL_IF_IP=$local_ip
+export GLOO_SOCKET_IFNAME=$nic_name
+export TP_SOCKET_IFNAME=$nic_name
+export HCCL_SOCKET_IFNAME=$nic_name
+
+export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libjemalloc.so.2:$LD_PRELOAD
+echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+sysctl -w vm.swappiness=0
+sysctl -w kernel.numa_balancing=0
+sysctl kernel.sched_migration_cost_ns=50000
+export VLLM_RPC_TIMEOUT=3600000
+export VLLM_EXECUTE_MODEL_TIMEOUT_SECONDS=30000
+# AIV
+export HCCL_OP_EXPANSION_MODE="AIV"
+export PYTORCH_NPU_ALLOC_CONF=expandable_segments:True
+export OMP_PROC_BIND=false
+export OMP_NUM_THREADS=1
+export TASK_QUEUE_ENABLE=1
+export ASCEND_BUFFER_POOL=4:8
+export LD_LIBRARY_PATH=/usr/local/Ascend/ascend-toolkit/latest/python/site-packages/mooncake:$LD_LIBRARY_PATH
+
+export HCCL_BUFFSIZE=1500
+export VLLM_ASCEND_ENABLE_FLASHCOMM1=1
+export ASCEND_RT_VISIBLE_DEVICES=$1
+
+export VLLM_ASCEND_ENABLE_FUSED_MC2=1
+export DYNAMIC_EPLB="true"
+
+vllm serve /mnt/weight/kimi25_w4a8_static_m6 \
+    --host 0.0.0.0 \
+    --port $2 \
+    --data-parallel-size $3 \
+    --data-parallel-rank $4 \
+    --data-parallel-address $5 \
+    --data-parallel-rpc-port $6 \
+    --tensor-parallel-size $7 \
+    --enable-expert-parallel \
+    --seed 1024 \
+    --quantization ascend \
+    --safetensors-load-strategy 'prefetch' \
+    --served-model-name kimi25 \
+    --trust-remote-code \
+    --max-num-seqs 4 \
+    --max-model-len 8192 \
+    --max-num-batched-tokens 8192 \
+    --no-enable-prefix-caching \
+    --gpu-memory-utilization 0.88 \
+    --enforce-eager \
+    --additional-config '{"eplb_config": {"dynamic_eplb": true, "expert_heat_collection_interval": 200},"ascend_compilation_config": {"enable_static_kernel": true}}' \
+    --profiler-config '{"profiler": "torch", "torch_profiler_dir": "/mnt/share/s30078275/profiling_p", "torch_profiler_with_stack": false}' \
+    --speculative-config '{"method": "dflash","model": "/mnt/weight/k2.5_dflash", "num_speculative_tokens": 3}' \
+    --kv-transfer-config \
+    '{"kv_connector": "MooncakeConnectorV1",
+    "kv_role": "kv_producer",
+    "kv_port": "30000",
+    "engine_id": "0",
+    "kv_connector_extra_config": {
+                "prefill": {
+                        "dp_size": 2,
+                        "tp_size": 8
+                },
+                "decode": {
+                        "dp_size": 32,
+                        "tp_size": 1
+                }
+        }
+    }'\
+    2>&1 | tee 2p1d_p1.log
